4483c09dbc1e4bfbae57131e05946fe4a5454b57
Chano Im <sumdolun@yahoo.co.jp>
2017-02-12 23:01:25 +0900

    add runner

---
+++ Beagle/Assets/script/Block_Scroll.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Block_Scroll : MonoBehaviour {
+
+	public GameObject Block;
+	public float speed = 3;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Block.transform.Translate (Vector3.left * speed * Time.deltaTime);
+
+//		string TagName = Block.tag.ToString();
+//
+//		Debug.Log ("Object Tag Name:" + TagName);
+
+		float BlockPosition = Block.transform.position.x;
+		if (BlockPosition <= -20) {
+			Vector3 temp = new Vector3(40.0f,0,0);
+			Block.transform.position += temp;
+		}
+		Debug.Log ("BlockPosition:" + BlockPosition);	
+	}
+}
---
+++ Beagle/Assets/script/Player.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+//	// Use this for initialization
+//	void Start () {
+//	
+//	}
+	
+	// Update is called once per frame
+	void Update () {
+//		Debug.Log ("Update!");
+		if (Input.GetKeyDown(KeyCode.Space)) {
+			Jump ();
+		} else if (Input.GetKeyDown(KeyCode.RightArrow)) {
+			GoForward ();
+		} else if (Input.GetKeyDown(KeyCode.LeftArrow)) {
+			GoBackward ();
+		}
+	}
+
+	void Jump () {
+		Debug.Log ("Player Jump");
+	}
+
+	void GoForward () {
+		Debug.Log ("Player GoForward");
+	}
+
+	void GoBackward () {
+		Debug.Log ("Player GoBackward");
+	}
+}
